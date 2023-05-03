1749e00b2918ed79cd46231eb56d0a62c54b3de2
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2023-05-03 13:46:48 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Camaradinha</h1>
+      <h2>Pronto pra primeira aula</h2>
+    </div>
+  );
+}
